07ca7a18e68c750ca18daa6884b07d49ecb1f38a
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-09 23:54:33 -0300

    add function localization

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,9 +1,46 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
 import  MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
 
 function Main() {
-    return <MapView style={{ flex: 1 }}/>
+    const [currentRegion, setCurrentRegion] = useState(null);
+    // pegando a localização do usuario caso for permitido
+    useEffect(() => {
+        async function loadInitialPosition() {
+            const { granted } = await requestPermissionsAsync();
+
+            if(granted) {
+                const { coords } = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const { latitude, longitude } = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.04,
+                    longitudeDelta: 0.04,
+                })
+            }
+        }
+
+        loadInitialPosition();
+    }, []);
+
+    if(!currentRegion) {
+        return null;
+    }
+
+    return <MapView initialRegion={currentRegion} style={StyleSheet.map}/>
 }
+
+const style = StyleSheet.create({
+    map: {
+         flex: 1
+    },
+})
 
 export default Main;
