af148bd345325f25287ba41e8626c0fdf82575ac
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-11 01:35:40 -0300

    add imagem on map

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import  MapView from 'react-native-maps';
+import { StyleSheet, Image } from 'react-native';
+import  MapView, { Marker } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 
@@ -34,13 +34,27 @@ function Main() {
         return null;
     }
 
-    return <MapView initialRegion={currentRegion} style={styles.map}/>
+    return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={currentRegion} >
+            <Image style={styles.avatar} source={{ uri: 'https://avatars1.githubusercontent.com/u/42756551?s=460&u=e4475ddca2f9c544b67ea619c636d50512d54be0&v=4' }} />
+        </Marker>
+    </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
     map: {
          flex: 1
     },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#FFF',
+    }
 })
 
 export default Main;
