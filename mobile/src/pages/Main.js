385e7ad0d4e9f2f93b53eed2566cf39d582cf61a
Ricardo Carvalho Santos <42756551+RicardoEstudante@users.noreply.github.com>
2020-06-12 21:07:23 -0300

    add finish vizualization of mobile

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -94,7 +94,48 @@ const styles = StyleSheet.create({
     devBio: {
         color: '#666',
         marginTop: 5,
-    }
+    },
+
+    devTechs: {
+        marginTop: 5
+    },
+
+    searchForm: {
+        position: 'absolute',
+        bottom: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        textShadowOffset: {
+            width: 4,
+            height: 4,
+        }, 
+        
+        elevation: 2,
+    },
+
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15.
+    },
 })
 
 export default Main;
